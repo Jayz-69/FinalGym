96de0cb749cdc0e70ef75ea5d09d37e1b612d574
jay <jay.anjarlekar@cloverinfotech.com>
2025-08-06 15:21:47 +0530

    Commit to check multiple commits

---
+++ gym_management/jim/doctype/clover_infotech/clover_infotech.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Jayz and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Clover Infotech', {
+	// refresh: function(frm) {
+
+	// }
+});
